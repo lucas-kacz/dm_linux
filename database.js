7fc6942cdd9085d198a10c01fd7bb676466d6664
Lucas Kaczmarski <113424948+lucas-kacz@users.noreply.github.com>
2022-12-27 20:15:24 +0100

    Update database.js

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 const mysql = require ('mysql');
 
 var connection = mysql.createConnection({
-    host: database-1.cnx76zh3lsox.eu-west-3.rds.amazonaws.com,
+    host: "database-1.cnx76zh3lsox.eu-west-3.rds.amazonaws.com",
     user: "root",
-    password: EU(f5*qNthORPHMhoy9Qf[+<mT$5,
-    database: binance_api,
+    password: "EU(f5*qNthORPHMhoy9Qf[+<mT$5",
+    database: "binance_api",
 });
 
 connection.connect(function(error){
